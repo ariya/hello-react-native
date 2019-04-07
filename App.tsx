39a07356d584ede56540fd90e306d1bc641e27ee
Ariya Hidayat <ariya.hidayat@gmail.com>
2019-04-07 05:03:58 -0700

    Add minimalistic TSLint rules

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import 'react-native';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.

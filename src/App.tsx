89930b7d5fb454f70b65c012e227cc6b228e8e1d
Ariya Hidayat <ariya.hidayat@gmail.com>
2019-09-11 21:33:10 -0700

    Show the type of the JavaScript engine being used

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import {StyleSheet, Text, View} from 'react-native';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    const useHermes = global.hasOwnProperty('HermesInternal');
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native v0.60!</Text>
+        <Text>
+          {useHermes
+            ? 'JavaScript engine: Hermes'
+            : 'JavaScript engine: JavaScriptCore'}
+        </Text>
       </View>
     );
   }

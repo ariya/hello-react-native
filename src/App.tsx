a26862123a9d0c697c7446b40af4ca3c1f66a792
Ariya Hidayat <ariya.hidayat@gmail.com>
2020-12-29 12:35:06 -0800

    Tweak the welcome text

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native {RNVersion.version.minor}
+          This is a demo using React Native {RNVersion.version.minor}
         </Text>
         <Text>
           {useHermes
